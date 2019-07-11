94b4cd1ecad2fcefb7613caa87f19fcfe0c93131
Aarushi Agrawal <aarushi.agrawal77@gmail.com>
2019-07-11 10:05:21 -0700

    added gradient to login screen

--- app/src/main/java/com/example/parsetagram/MainActivity.java
+++ app/src/main/java/com/example/parsetagram/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.parsetagram;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -8,6 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.parse.LogInCallback;
 import com.parse.ParseException;
@@ -30,6 +32,11 @@ public class MainActivity extends AppCompatActivity {
             finish();
         } else {
             setContentView(R.layout.activity_main);
+            ConstraintLayout constraintLayout = findViewById(R.id.layout);
+            AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+            animationDrawable.setEnterFadeDuration(2000);
+            animationDrawable.setExitFadeDuration(4000);
+            animationDrawable.start();
 
             usernameInput = findViewById(R.id.usernameInput);
             passwordInput = findViewById(R.id.passwordInput);
